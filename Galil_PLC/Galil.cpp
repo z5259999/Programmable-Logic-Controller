d12d163b695408fa16b76033042993cedb666750
George Kamateros <g.kamateros@student.unsw.edu.au>
2021-11-12 13:49:02 +1100

    Set-up skeleton code

--- Galil_PLC/Galil.cpp
+++ Galil_PLC/Galil.cpp
@@ -24,6 +24,7 @@ Galil::~Galil() {
 }
 
 /////////////////////////////////DIGITAL OUTPUTS/////////////////////////////////
+
 void Galil::DigitalOutput(uint16_t value) {
 
 }
@@ -36,9 +37,71 @@ void Galil::DigitalBitOutput(bool val, uint8_t bit) {
 
 }
 
-/////////////////////////////////DIGITAL INPUTS/////////////////////////////////
-
-
-
 
 /////////////////////////////////DIGITAL INPUTS/////////////////////////////////
+
+uint16_t Galil::DigitalInput() {
+
+}
+
+uint8_t Galil::DigitalByteInput(bool bank) {
+
+}
+
+bool Galil::DigitalBitInput(uint8_t bit) {
+
+}
+
+bool Galil::CheckSuccessfulWrite() {
+
+}
+
+
+/////////////////////////////////ANALOG FUNCTIONS/////////////////////////////////
+
+float Galil::AnalogInput(uint8_t channel) {
+
+}
+
+void Galil::AnalogOutput(uint8_t channel, double voltage) {
+
+}
+
+void Galil::AnalogInputRange(uint8_t channel, uint8_t range) {
+
+}
+
+
+//////////////////////////ENCODER / CONTROLLER FUNCTIONS////////////////////////////
+
+void Galil::WriteEncoder() {
+
+}
+
+int Galil::ReadEncoder() {
+
+}
+
+void Galil::setSetPoint(int s) {
+
+}
+
+void setKp(double gain) {
+
+}
+
+void Galil::setKi(double gain) {
+
+}
+
+void Galil::setKd(double gain) {
+
+}
+
+void Galil::PositionControl(bool debug, int Motorchannel) {
+
+}
+
+void Galil::SpeedControl(bool debug, int Motorchannel) {
+
+}
