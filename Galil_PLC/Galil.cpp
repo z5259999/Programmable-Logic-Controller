d001d72cfe84fd431edfbe467d1db1c7ea8d0d2e
George Kamateros <g.kamateros@student.unsw.edu.au>
2021-11-20 19:48:15 +1100

    Scrumble

--- Galil_PLC/Galil.cpp
+++ Galil_PLC/Galil.cpp
@@ -13,23 +13,100 @@
 
 Galil::Galil() {
 
+	// Default constructor. Initialize variables, open Galil connection and allocate memory. NOT AUTOMARKED
+
+	Functions = new EmbeddedFunctions;
+	g = 0;
+	setPoint = 0;
+	
+	for (int i = 0; i < 1024; i++) {
+		ReadBuffer[i] = {NULL};
+	}
+
+	for (int i = 0; i < 3; i++) {
+		ControlParameters[i] = 0;
+	}
+
 }
 
 Galil::Galil(EmbeddedFunctions* Funcs, GCStringIn address) {
+	
+	// Constructor with EmbeddedFunciton initialization
+
+	// Why is it different?
+	Functions = Funcs;
+	g = 0;
+	setPoint = 0;
+	
+	for (int i = 0; i < 1024; i++) {
+		ReadBuffer[i] = {NULL};
+	}
+
+	for (int i = 0; i < 3; i++) {
+		ControlParameters[i] = 0;
+	}
+	
+	GReturn status = 0;
+	status = Functions->GOpen(address, &g);
+
+	// Check Connection Status
+	if (status == G_NO_ERROR) {
+		std::cout << "Connected!" << std::endl;
+	}
+	else {
+		std::cout << "Nope, Try Again!" << std::endl;
+	}
 
 }
 
 Galil::~Galil() {
+	
+	if (g) {
+		
+		// Shut it down and delete it all
+		Functions->GClose(g);
+		delete Functions;
+	}
 
+	std::cout << "Goodbye!" << std::endl;
 }
 
 /////////////////////////////////DIGITAL OUTPUTS/////////////////////////////////
 
+// Write to all 16 bits of digital output, 1 command to the Galil
 void Galil::DigitalOutput(uint16_t value) {
+
+	char Command[128] = "";
+	ReadBuffer[0] = {NULL};
+
+	// Lowest Byte only, no need to extract/split
+	if (value <= UINT8_MAX) {
+		sprintf_s(Command, "OP %d", value);
+	}
+
+	// Uses all 16 bits, requires bit shifting to extract
+	else if ((value > UINT8_MAX) && (value <= UINT16_MAX)) {
+		int byteLower = value & 0xFF;
+		int byteHigher = value >> 8;
+		sprintf_s(Command, "OP %d, %d", byteLower, byteHigher);
+	}
+	
+	Functions->GCommand(g, Command, ReadBuffer, sizeof(ReadBuffer), 0);
 
 }
 
 void Galil::DigitalByteOutput(bool bank, uint8_t value) {
+
+	char Command[128] = "";
+	ReadBuffer[0] = { NULL };
+
+	if (bank == 0) {
+		sprintf_s(Command, "OP %d", value);
+	}
+
+	else {
+
+	}
 
 }
 
@@ -76,6 +153,8 @@ void Galil::AnalogInputRange(uint8_t channel, uint8_t range) {
 
 void Galil::WriteEncoder() {
 
+
+
 }
 
 int Galil::ReadEncoder() {
@@ -83,25 +162,17 @@ int Galil::ReadEncoder() {
 }
 
 void Galil::setSetPoint(int s) {
-
+	setPoint = s;
 }
 
-void setKp(double gain) {
-
+void Galil::setKp(double gain) {
+	ControlParameters[0] = gain;
 }
 
 void Galil::setKi(double gain) {
-
+	ControlParameters[1] = gain;
 }
 
 void Galil::setKd(double gain) {
-
-}
-
-void Galil::PositionControl(bool debug, int Motorchannel) {
-
-}
-
-void Galil::SpeedControl(bool debug, int Motorchannel) {
-
+	ControlParameters[2] = gain;
 }
